d2620bbd1e3e952290aa4122c3ef6b9f322f1838
KrasimirHristov <krasimir_2001@abv.bg>
2020-10-08 06:25:16 +0000

    comment

--- src/exercise2/DayOfWeek.java
+++ src/exercise2/DayOfWeek.java
@@ -1,6 +1,6 @@
 package exercise2; 
 
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class DayOfWeek {
 
@@ -10,31 +10,31 @@ public class DayOfWeek {
        System.out.print("Enter a number: ");
        int day = input.nextInt();
        System.out.print("Day of week: ");
-       if (day == 1)
+       if(day == 1)
        {
          System.out.print("Monday");
        }
-       else if (day == 2);
+       else if(day == 2)
        {
          System.out.print("Tuesday");
        }
-       else if (day == 3);
+       else if(day == 3)
        {
          System.out.print("Wednesday");
        }
-       else if (day == 4)
+       else if(day == 4)
        {
          System.out.print("Thursday");
        }
-       else if (day == 5)
+       else if(day == 5)
        {
          System.out.print("Friday");
        }
-       else if (day == 6)
+       else if(day == 6)
        {
          System.out.print("Saturday");
        }
-       else if (day == 7)
+       else if(day == 7)
        {
          System.out.print("Sunday");
        }
