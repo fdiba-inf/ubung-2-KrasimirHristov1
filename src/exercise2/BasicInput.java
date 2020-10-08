bb55bdca1c8e9abedde71d658da9fedb048e83d8
KrasimirHristov <krasimir_2001@abv.bg>
2020-10-08 06:19:18 +0000

    komment

--- src/exercise2/BasicInput.java
+++ src/exercise2/BasicInput.java
@@ -2,9 +2,10 @@ package exercise2;
 
 import java.util.Scanner;
 
-public class BasicInput {
+public class 
+BasicInput {
 
-    public static void main(String[] args) {
+     {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
